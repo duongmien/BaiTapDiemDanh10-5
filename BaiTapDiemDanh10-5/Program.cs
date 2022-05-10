f7f7ca88054593eee8428eaced74eb06f669f428
duongmien <duongthimien7@gmail.com>
2022-05-10 07:35:27 +0700

    Add project files.

---
+++ BaiTapDiemDanh10-5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapDiemDanh10_5
+{
+    class Program
+    {
+       public static void nhapSo(int so_234[])
+        {
+            for (int i = 0; i <= 11; i++)
+            {
+                Console.WriteLine("Nhap so thu " + (i + 1));
+                int a_234 = int.Parse(Console.ReadLine());
+
+            }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
