f4178e9edff23aa117d7a59a6213823540319417
duongmien <duongthimien7@gmail.com>
2022-05-10 07:57:06 +0700

    update

--- BaiTapDiemDanh10-5/Program.cs
+++ BaiTapDiemDanh10-5/Program.cs
@@ -20,18 +20,35 @@ namespace BaiTapDiemDanh10_5
             Console.WriteLine("Max la: " + max_234 + "\nMin la: " + min_234);
         }
 
-        public static void count(int[] st_234)
+        int Timsolanxuathien(int [] so_234)
         {
+            int max_234 = 0;
+            int dem_234 = 1;
             for (int i = 0; i < 10; i++)
             {
+                if (so_234[i] == so_234[i + 1])
+                {
+                    dem_234++;
+                    if (dem_234 > max_234)
+                    {
+                        max_234 = dem_234;
+                    }
 
+                }
+                else
+                {
+
+                    dem_234 = 1;
+                }
             }
+            return max_234;
+
         }
 
-        public static void sapXep(int[] st_234)
+        public static void sapXepGiamDan(int[] st_234)
         {
             int tg;
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 10; i++)
                 for (int j = i + 1; j < 10; j++)
                     if (st_234[i] < st_234[j])
                     {
@@ -41,8 +58,11 @@ namespace BaiTapDiemDanh10_5
                     }
             Console.Write("Sap xep giam dan: ");
             xuat(st_234);
-
-            for (int i = 0; i < 9; i++)
+        }
+        public static void sapXepTangDan(int[] st_234)
+        {
+            int tg;
+            for (int i = 0; i < 10; i++)
                 for (int j = i + 1; j < 10; j++)
                     if (st_234[i] > st_234[j])
                     {
@@ -50,7 +70,7 @@ namespace BaiTapDiemDanh10_5
                         st_234[i] = st_234[j];
                         st_234[j] = tg;
                     }
-            Console.Write("\nSap xep tang dan: ");
+            Console.Write("Sap xep tang dan: ");
             xuat(st_234);
         }
         public static void xuat(int[] st_234)
@@ -63,17 +83,18 @@ namespace BaiTapDiemDanh10_5
         static void Main(string[] args)
         {
             int[] so_234 = new int[10];
-            for (int i = 0; i <= 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Nhap so thu " + (i + 1));
                 so_234[i] = int.Parse(Console.ReadLine());
             }
             minMax(so_234);
             Console.WriteLine();
-            sapXep(so_234);
-
+            sapXepGiamDan(so_234);
+            sapXepTangDan(so_234);
+            int Nhieunhat = Timsolanxuathien(so_234);
+            Console.WriteLine("\nSo lan nhat hien nhieu nhat la: %d", Nhieunhat);
             Console.ReadLine();
-
         }
     }
 }
